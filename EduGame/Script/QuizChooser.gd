9afcae6d2e290555cd2171ed240e102d88afcae8
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2023-10-21 07:15:39 -0600

    Update QuizChooser.gd

--- EduGame/Script/QuizChooser.gd
+++ EduGame/Script/QuizChooser.gd
@@ -67,13 +67,10 @@ func _on_http_request_request_completed(result, response_code, headers, body):
 
 func _on_http_request_done():
 	# Continue your code after the HTTP request here
-<<<<<<< Updated upstream
 	# Load the Quiz
-	get_tree().change_scene_to_file('res://Scenes/quiz.tscn')
-=======
 	#get_tree().change_scene_to_file('res://Scenes/classroom.tscn')
 	get_tree().change_scene_to_file('res://Scenes/classroom.tscn')
->>>>>>> Stashed changes
+
 	self.disconnect("http_request_completed", Callable(self, "_on_http_request_done"))
 	# self.disconnect("http_request_completed", self, "_on_http_request_done")
 
