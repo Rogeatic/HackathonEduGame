e6ce8ccd40215c217cc931112e6bece88f35ae93
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2023-10-21 02:08:31 -0600

    need to clean up

--- EduGame/Script/GlobalData.gd
+++ EduGame/Script/GlobalData.gd
@@ -6,3 +6,32 @@ var pos_subjects_ = ["Math", "Science", "English"]
 var subject_studying_
 
 var json_data_
+var json_wrong_ = {"results":[]}
+
+func getFirstQuestion():
+	if (len(json_data_["results"]) > 0):
+		print (json_data_)
+		return json_data_["results"][0]["question"]
+	else:
+		print("outOfQuestions")
+		return null
+
+#func removeFirstQuestion():
+#	if (len(json_data_["results"]) > 0):
+#		print ("removing")
+#		json_carrier_["results"].append(json_data_["results"][0]["question"])
+#		json_data_["results"].remove_at(0)
+#		return json_carrier_["results"][len(json_carrier_["results"])-1]
+#	else:
+#		print("outOfQuestions")
+#		return null
+
+func removeFirstQuestion():
+	if (len(json_data_["results"]) > 0):
+		# print ("removing")
+		json_wrong_["results"].append(json_data_["results"][0]["question"])
+		# json_data_["results"].remove_at(0)
+		return json_wrong_["results"][len(json_wrong_["results"])-1]
+	else:
+		print("outOfQuestions")
+		return null
