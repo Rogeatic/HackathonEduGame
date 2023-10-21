232073647f824a6175f995ce7ed48a342ee2a91f
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2023-10-21 12:12:03 -0600

    updated the quiz chooser, don't load lvl

--- EduGame/Script/GlobalData.gd
+++ EduGame/Script/GlobalData.gd
@@ -17,7 +17,7 @@ var difficultyList_ = [
 	"hard"
 ]
 
-<<<<<<< Updated upstream
+
 var level_scene = preload("res://Scenes/Level.tscn")
 var player_scene = preload("res://Scenes/Player.tscn")
 
@@ -31,7 +31,7 @@ var color: Array = ["blue", "grey", "orange"]
 
 # Level variables
 var current_level = 1
-=======
+
 var htmlEntitesReplacement_ = {
 	"&#039;": "'",
 	"&Sigma;": "Σ",
@@ -41,7 +41,7 @@ var htmlEntitesReplacement_ = {
 	"&quot;": '"',
 	"&rsquo;": "’"
 }
->>>>>>> Stashed changes
+
 
 var type_of_question_ = ["multiple", "boolean"]
 var number_of_quesions_ = 25
