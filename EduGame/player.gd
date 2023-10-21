5f91250448452f781f45cdb34bc6187b3f27c518
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2023-10-20 18:46:20 -0600

    Added Player Script

---
+++ EduGame/player.gd
@@ -0,0 +1,12 @@
+extends KinematicCollision2D 
+
+
+func read_input():
+	if Input.is_action_pressed("up"):
+		pass
+	if Input.is_action_pressed("down"):
+		pass
+	if Input.is_action_pressed("left"):
+		pass
+	if Input.is_action_pressed("right"):
+		pass
